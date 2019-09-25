f6eb98892c37a36980cb1a341df8374b1a782797
Adrian Sims <adriansims62781@gmail.com>
2019-09-25 22:58:28 +0200

    first commit

---
+++ build-layout.bat
@@ -0,0 +1,9 @@
+vs_buildtools --layout c:\temp\vsbuildtools2019  ^
+--lang en-US ^
+--includeOptional --includeRecommended ^
+--add Microsoft.VisualStudio.Workload.DataBuildTools ^
+--add Microsoft.VisualStudio.Workload.ManagedDesktopBuildTools ^
+--add Microsoft.VisualStudio.Workload.MSBuildTools ^
+--add Microsoft.VisualStudio.Workload.NetCoreBuildTools ^
+--add Microsoft.VisualStudio.Workload.VCTools ^
+--add Microsoft.VisualStudio.Workload.VisualStudioExtensionBuildTools
